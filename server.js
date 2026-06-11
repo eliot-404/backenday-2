36311df4a2b6d34e2a0b02afe08cf09f42d66ac9
Eliot Elderson <rizwan687md@gmail.com>
2026-06-12 01:07:43 +0530

    update routes

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express")
 
 const app = express()
  // server instance create karna 
+
+ app.get("/", (req, res) => {
+
+    res.send("Hello world") // jab bhi /about page pe jayenge to This is about page show hoga 
+})
+
  
 app.get("/about", (req, res) => {
 
     res.send("This is about page") // jab bhi /about page pe jayenge to This is about page show hoga 
 })
-app.listen(3000) //server stsrt ho rha h 
+
+
+
+app.get("/home" , (req, res) => {
+    res.send("This is home page")
+})
+app.listen( process.env.PORT||3000) //server stsrt ho rha h 
